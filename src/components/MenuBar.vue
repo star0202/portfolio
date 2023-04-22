957b2402c9599243c94b883a71b03544b429d7e0
Starcea <stardev.uwu@gmail.com>
2023-04-22 18:57:20 +0900

    feat: simple MenuBar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="bg-black bg-opacity-50 h-12 px-4 flex items-center terminal">
-    Starcea@StarPad:~$
+  <div class="bg-black bg-opacity-50 h-12 px-4 flex items-center">
+    Starcea
     <div class="flex-grow" />
     <a href="https://github.com/star0202" target="_blank" rel="noreferrer">
       <font-awesome-icon :icon="['fab', 'github']" style="font-size: 18px" />
     </a>
   </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped>
-.terminal {
-  font-family: 'JetBrains Mono', monospace;
-  font-size: 14px;
-}
-</style>

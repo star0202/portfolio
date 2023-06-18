6efac6b88beef207c84a19c3b5e2df2ec95ebe43
Starcea <stardev.uwu@gmail.com>
2023-06-18 12:11:23 +0900

    Revert "feat: redirect to detailed md"
    This reverts commit d129a5cd11cb0fbba76dd9f4cee211961e21b26f.

--- src/windows/Detailed.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <meta
-    http-equiv="refresh"
-    content="0; url=https://github.com/star0202/star0202/blob/main/detailed.md"
-  />
-  <div class="text-center">Redirecting...</div>
-</template>
